de827ffd3b5e6dcb0a7b722952d104e9b3c1ff4f
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-08-02 23:19:04 +0600

    jwt & login ctrl done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,4 @@
+const { createToken } = require("../helpers/token.helper");
 const User = require("../models/user.model");
 
 // Signup
@@ -7,13 +8,27 @@ const signupUser = async (req, res) => {
 
     const user = await User.signup(name, email, password);
 
-    res.status(200).json(user);
+    const token = createToken(user._id);
+
+    res.status(200).json({ user, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
 // Login
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.login(email, password);
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = { loginUser, signupUser };
